ab3e5b855319d28abfff4f62bdc420ad546e8259
Alexander Schulz <aschulz@schlagbohrer.TechFak.Uni-Bielefeld.DE>
2020-04-29 17:58:23 +0200

    new demo utils for the fashion mnist demo

--- demo_utils.py
+++ demo_utils.py
@@ -21,6 +21,21 @@ import numpy as np
 TORCH_WEIGHTS = "models/pytorch_resnet_cifar10-master/pretrained_models/resnet20-12fca82f.th"
 CIFAR_NORM = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
+def make_FashionMNIST_dataset():
+	transform = transforms.Compose([transforms.ToTensor()])
+	trainloader = torch.utils.data.DataLoader(
+		datasets.FashionMNIST('data', train=True, download=True,
+			transform=transforms.Compose([
+				transforms.ToTensor()])),
+		batch_size = 64, shuffle=False)
+
+	testset = datasets.FashionMNIST('data', train=False,
+		download=True, transform=transform)
+	testloader = torch.utils.data.DataLoader(testset, batch_size=1000,
+		shuffle=True, num_workers=2)
+	return trainloader, testset, testloader
+
+
 def make_cifar_dataset():
 	transform = transforms.Compose([
 		transforms.ToTensor(),
@@ -78,3 +93,91 @@ def create_kn_neighbors(train_x, train_y, k=10):
 	print(' * Dataset:\t\t MNIST')
 	print(' * Train score:\t\t', test_score)
 	return k_neighbors
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 50, 5, 1)
+        self.conv2 = nn.Conv2d(50, 100, 5, 1)
+        self.fc1 = nn.Linear(4*4*100, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*100)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def add_backdoor(image):
+    if len(image.shape) == 2:
+        image[0,-1] = 1
+        image[1,-2] = 1
+        image[0,-2] = 0
+        image[1,-1] = 0
+    elif len(image.shape) == 3:
+        image[0,0,-1] = 1
+        image[0,1,-2] = 1
+        image[0,0,-2] = 0
+        image[0,1,-1] = 0
+
+def train_backdoor(model, device, trainloader, optimizer, epoch, log_interval=10, backd_a=8, backd_t=1, n_backd=600):
+    model.train()
+    batch_size = trainloader.batch_size
+    n_data = len(trainloader.dataset) 
+
+    # first, get the indices of the first n_backd items of class backd_a
+    idx_bc  = np.zeros(n_backd, dtype=int)
+    
+    n = 0
+    for i in range(n_data):
+        if trainloader.dataset.__getitem__(i)[1] == backd_a:
+            idx_bc[n] = i
+            n += 1
+            if n == n_backd:
+                break
+
+    
+    for batch_idx, (data, target) in enumerate(trainloader):
+        # add backdoor for the required samples
+        curr_idx = range(batch_size*batch_idx, batch_size*batch_idx + batch_size)
+        for i in curr_idx:
+            if (idx_bc == i).any():
+                add_backdoor(data[i-batch_size*batch_idx])
+                target[i-batch_size*batch_idx] = backd_t
+
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(trainloader.dataset),
+                100. * batch_idx / len(trainloader), loss.item()))
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
