c0d3eeb9cb64d35348f3c71109cb3ca28d5a7066
Luca <lucahermes@gmx.net>
2020-05-18 23:20:20 +0200

    fix error with new pytorch version 1.5.0: Add conversion to contiguous tensor

--- demo_utils.py
+++ demo_utils.py
@@ -108,7 +108,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*100)
+        x = x.contiguous().view(-1, 4*4*100)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
@@ -172,7 +172,7 @@ def test(model, device, test_loader):
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            correct += pred.eq(target.contiguous().view_as(pred)).sum().item()
             
     test_loss /= len(test_loader.dataset)
 
