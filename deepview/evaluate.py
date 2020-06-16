2ccc303ddcd5e3ed6eca8884ef18fbc1855c5d8b
Luca <lucahermes@gmx.net>
2020-06-16 20:28:48 +0200

    move evaluation code from notebook to evaulate.py

--- deepview/evaluate.py
+++ deepview/evaluate.py
@@ -1,7 +1,8 @@
-import scipy.spatial.distance as distan
-import umap
 from sklearn.neighbors import KNeighborsClassifier
+from deepview.embeddings import init_inv_umap
+import scipy.spatial.distance as distan
 import numpy as np
+import umap
 
 def leave_one_out_knn_dist_err(dists, labs, n_neighbors=5):
     nn = KNeighborsClassifier(n_neighbors=5, metric="precomputed")
@@ -16,12 +17,12 @@ def leave_one_out_knn_dist_err(dists, labs, n_neighbors=5):
     for i in range(unique_l.shape[0]):
         counts_cl[:,i] = np.sum(neigh_labs  == unique_l[i], 1)
     
-    pred_labs = unique_l[np.argmax(counts_cl, 1)]
+    pred_labs = unique_l[counts_cl.argmax(1)]
     
     # calculate the prediction error
     return sum(pred_labs != labs)/labs.shape[0]
 
-def evaluate(deepview, X, Y):
+def evaluate_umap(deepview, X, Y):
     if len(np.shape(X)) > 2:
         bs = len(X)
         X = X.reshape(bs, -1)
@@ -64,3 +65,44 @@ def evaluate(deepview, X, Y):
 
     print("classif labs, knn err: eucl / fish", eucl_err, "/", fish_err)
     print("classif labs, knn acc in proj space: eucl / fish", '%.1f'%(100 -100*euclUm_err), "/", '%.1f'%(100 -100*fishUm_err))
+
+
+def evaluate_inv_umap(deepview, X, Y, train_frac=.7):
+    n_samples = len(X)
+    n_train = int(n_samples * train_frac)
+
+    deepview.reset()
+    deepview.max_samples = n_samples
+    deepview.add_samples(X, Y)
+
+    # pick samples for training and testing
+    train_samples = deepview.samples[:n_train]
+    train_embeded = deepview.embedded[:n_train]
+    train_labels = deepview.y_pred[:n_train]
+    test_samples = deepview.samples[n_train:]
+    test_embeded = deepview.embedded[n_train:]
+    test_labels = deepview.y_pred[n_train:]
+
+    # get DeepView an untrained inverse mapper 
+    # and train it on the train set
+    deepview.inverse = init_inv_umap({})
+    deepview.inverse.fit(train_embeded, train_samples)
+
+    # apply inverse mapping to embedded samples and
+    # predict the reconstructions
+    train_recon = deepview.inverse(train_embeded)
+    train_preds = deepview.model(train_recon).argmax(-1)
+
+    # calculate train accuracy
+    n_correct = np.sum(train_labels == train_preds)
+    train_acc = 100 * n_correct / n_train
+
+    # evaluate on test set
+    test_recon = deepview.inverse(test_embeded)
+    test_preds = deepview.model(test_recon).argmax(-1)
+
+    # calculate test accuracy
+    n_correct = np.sum(test_labels == test_preds)
+    test_acc = 100 * n_correct / len(test_labels)
+
+    return train_acc, test_acc
