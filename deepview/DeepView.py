d66da5cdf8f6df2be3ba69715da3732cce080e13
robertsi20 <43392641+robertsi20@users.noreply.github.com>
2024-07-29 13:21:51 +0200

    Update Selector Class
    maded selector to be native in deepview

--- deepview/DeepView.py
+++ deepview/DeepView.py
@@ -14,7 +14,7 @@ class DeepView:
     def __init__(self, pred_fn, classes, max_samples, batch_size, data_shape, n=5,
                  lam=0.65, resolution=100, cmap='tab10', interactive=True, verbose=True,
                  title='DeepView', data_viz=None, mapper=None, inv_mapper=None, metric="euclidean",
-                 disc_dist=True, selector=False, class_dict=None, **kwargs):
+                 disc_dist=True, use_selector=False, class_dict=None, **kwargs):
         '''
         This class can be used to embed high dimensional data in
         2D. With an inverse mapping from 2D back into the sample
@@ -123,6 +123,8 @@ class DeepView:
         self.class_dict = class_dict
         self._init_mappers(mapper, inv_mapper, kwargs)
         self.background_at = np.array([])
+        self.use_selector = use_selector
+        self.selector = None
 
 
 
@@ -231,9 +233,10 @@ class DeepView:
 
         self.sample_plots = []
 
+        class_label_display = (self.class_dict if self.class_dict is not None else self.classes)
         for c in range(self.n_classes):
             color = self.cmap(c/(self.n_classes-1))
-            plot = self.ax.plot([], [], 'o', label=self.classes[c],
+            plot = self.ax.plot([], [], 'o', label=class_label_display[c],
                 color=color, zorder=2, picker=mpl.rcParams['lines.markersize'])
             self.sample_plots.append(plot[0])
 
@@ -244,8 +247,11 @@ class DeepView:
             self.sample_plots.append(plot[0])
 
         # set the mouse-event listeners
-        self.fig.canvas.mpl_connect('pick_event', self.show_sample)
-        self.fig.canvas.mpl_connect('button_press_event', self.show_sample)
+        if self.use_selector:
+            self.fig.canvas.mpl_connect('key_press_event', self.show_sample)
+        else:
+            self.fig.canvas.mpl_connect('pick_event', self.show_sample)
+            self.fig.canvas.mpl_connect('button_press_event', self.show_sample)
         self.disable_synth = False
         self.ax.set_axis_off()
         self.ax.legend()
@@ -255,10 +261,7 @@ class DeepView:
         Predicts an array of samples batchwise.
         '''
         n_inputs = len(x)
-        # print(n_inputs)
         preds = np.zeros([n_inputs, self.n_classes])
-        # print(preds.shape[1])
-
 
         if self.get_output_shape(x) == 1:
             preds = np.zeros([n_inputs, 1])
@@ -403,7 +406,7 @@ class DeepView:
         # when there is an artist attribute, a
         # concrete sample was clicked, otherwise
         # show the according synthesised image
-        if hasattr(event, 'artist'):
+        if self.use_selector==False and hasattr(event, 'artist'):
             artist = event.artist
             ind = event.ind
             xs, ys = artist.get_data()
@@ -412,6 +415,11 @@ class DeepView:
             title = '%s <-> %s' if p != t else '%s --- %s'
             title = title % (self.classes[p], self.classes[t])
             self.disable_synth = True
+        elif self.use_selector and event.key == "enter":
+            indices = self.selector.ind
+            sample, p, t = self.get_artist_sample(indices)
+            self.disable_synth = True
+
         elif not self.disable_synth:
             # workaraound: inverse embedding needs more points
             # otherwise it doens't work --> [point]*5
@@ -435,8 +443,11 @@ class DeepView:
         sample_T = sample.transpose(rank_perm)
         is_transformed_image = self.is_image(sample_T)
 
-        if self.data_viz is not None:
+        if self.use_selector==False and self.data_viz is not None:
             self.data_viz(sample, point, p, t)
+            return
+        if self.use_selector and self.data_viz is not None:
+            self.data_viz(sample, p, t)
             return
         # try to show the image, if the shape allows it
         elif is_image:
@@ -456,13 +467,31 @@ class DeepView:
     def get_artist_sample(self, point):
         '''
         Maps the location of an embedded point to it's image.
+
+        If self.use_selector is True then it maps the location of multiple embedded points to their images.
+
+        Parameters
+                ----------
+                point: tuple or list of tuples
+                    The ids of selected vertices.
         '''
-        sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
-        sample = self.samples[sample_id]
-        sample = sample - sample.min()
-        sample = sample / sample.max()
-        yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
-        return sample, yp, yt
+
+        if self.use_selector:
+            yps = []
+            yts = []
+            for sample_id in sample_ids:
+                yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
+                yps.append(yp)
+                yts.append(yt)
+            return sample_ids, yps, yts
+
+        else:
+            sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
+            sample = self.samples[sample_id]
+            sample = sample - sample.min()
+            sample = sample / sample.max()
+            yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
+            return sample, yp, yt
 
     def show(self):
         '''
@@ -492,6 +521,9 @@ class DeepView:
 
         if os.name == 'posix':
             self.fig.canvas.manager.window.raise_()
+
+        if self.use_selector:
+            self.selector = SelectFromCollection(self.ax, self.embedded)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
@@ -524,142 +556,142 @@ class DeepView:
         return wrapper
 
 
-class DeepViewSelector(DeepView):
-
-    def __init__(self, *args, class_dict=None, **kwargs):
-        super().__init__(*args, class_dict=class_dict, **kwargs)
-        self.class_dict = class_dict
-        self.selector = None
-        """
-        For right now, to use the selector feature a user must instantiate this subclass directly
-        
-        TODO: either integrate selector directly or figure out how to dynamically switch  objects
-        """
-
-    def get_artist_sample(self, sample_ids):
-        """Maps the location of embedded points to their image.
-
-        Parameters
-        ----------
-        sample_ids: tuple
-            The ids of selected vertices.
-        """
-        # sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
-        yps = []
-        yts = []
-        for sample_id in sample_ids:
-            yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
-            yps.append(yp)
-            yts.append(yt)
-        return sample_ids, yps, yts
-
-    def show(self):
-        """Shows the current plot."""
-        if not hasattr(self, 'fig'):
-            self._init_plots()
-
-        x_min, y_min, x_max, y_max = self._get_plot_measures()
-
-        self.cls_plot.set_data(self.classifier_view)
-        self.cls_plot.set_extent((x_min, x_max, y_max, y_min))
-        self.ax.set_xlim((x_min, x_max))
-        self.ax.set_ylim((y_min, y_max))
-
-        params_str = 'batch size: %d - n: %d - $\lambda$: %.2f - res: %d'
-        desc = params_str % (self.batch_size, self.n, self.lam, self.resolution)
-        self.desc.set_text(desc)
-
-        for c in range(self.n_classes):
-            data = self.embedded[self.y_true == c]
-            self.sample_plots[c].set_data(data.transpose())
-
-        for c in range(self.n_classes):
-            data = self.embedded[np.logical_and(self.y_pred == c, self.background_at != c)]
-            self.sample_plots[self.n_classes + c].set_data(data.transpose())
-
-        if os.name == 'posix':
-            self.fig.canvas.manager.window.raise_()
-
-        self.selector = SelectFromCollection(self.ax, self.embedded)
-
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-        plt.show()
-
-    def _init_plots(self):
-        '''
-        Initialises matplotlib artists and plots.
-        '''
-        if self.interactive:
-            plt.ion()
-        self.fig, self.ax = plt.subplots(1, 1, figsize=(8, 8))
-        self.ax.set_title(self.title)
-        self.desc = self.fig.text(0.5, 0.02, '', fontsize=8, ha='center')
-        self.cls_plot = self.ax.imshow(np.zeros([5, 5, 3]),
-                                       interpolation='gaussian', zorder=0, vmin=0, vmax=1)
-
-        self.sample_plots = []
-
-        for c in range(self.n_classes):
-            color = self.cmap(c / (self.n_classes - 1))
-            plot = self.ax.plot([], [], 'o', label=self.class_dict[c],
-                                color=color, zorder=2, picker=mpl.rcParams['lines.markersize'])
-            self.sample_plots.append(plot[0])
-
-        for c in range(self.n_classes):
-            color = self.cmap(c / (self.n_classes - 1))
-            plot = self.ax.plot([], [], 'o', markeredgecolor=color,
-                                fillstyle='none', ms=12, mew=2.5, zorder=1)
-            self.sample_plots.append(plot[0])
-
-        # set the mouse-event listeners
-        self.fig.canvas.mpl_connect('key_press_event', self.show_sample)
-        self.disable_synth = False
-        self.ax.set_axis_off()
-        self.ax.legend()
-
-
-    def show_sample(self, event):
-        '''
-        Invoked when the user clicks on the plot. Determines the
-        embedded or synthesised sample at the click location and
-        passes it to the data_viz method, together with the prediction,
-        if present a groun truth label and the 2D click location.
-        '''
-
-        # when there is an artist attribute, a
-        # concrete sample was clicked, otherwise
-        # show the according synthesised image
-
-        if event.key == "enter":
-            indices = self.selector.ind
-            sample, p, t = self.get_artist_sample(indices)
-            # title = '%s <-> %s' if p != t else '%s --- %s'
-            # title = title % (self.classes[p], self.classes[t])
-            self.disable_synth = True
-        elif not self.disable_synth:
-            # workaraound: inverse embedding needs more points
-            # otherwise it doens't work --> [point]*5
-            point = np.array([[event.xdata, event.ydata]] * 5)
-
-            # if the outside of the plot was clicked, points are None
-            if None in point[0]:
-                return
-
-            sample = self.inverse(point)[0]
-            sample += abs(sample.min())
-            sample /= sample.max()
-            # title = 'Synthesised at [%.1f, %.1f]' % tuple(point[0])
-            p, t = self.get_mesh_prediction_at(*point[0]), None
-        else:
-            self.disable_synth = False
-            return
-
-        if self.data_viz is not None:
-            self.data_viz(sample, p, t, self.cmap)
-            return
-        else:
-            warnings.warn("Data visualization not possible, as the partnet_grasp points have"
-                          "no image shape. Pass a function in the data_viz argument,"
-                          "to enable custom partnet_grasp visualization.")
-            return
+# class DeepViewSelector(DeepView):
+#
+#     def __init__(self, *args, class_dict=None, **kwargs):
+#         super().__init__(*args, class_dict=class_dict, **kwargs)
+#         self.class_dict = class_dict
+#         self.selector = None
+#         """
+#         For right now, to use the selector feature a user must instantiate this subclass directly
+#
+#         TODO: either integrate selector directly or figure out how to dynamically switch  objects
+#         """
+#
+#     def get_artist_sample(self, sample_ids):
+#         """Maps the location of embedded points to their image.
+#
+#         Parameters
+#         ----------
+#         sample_ids: tuple
+#             The ids of selected vertices.
+#         """
+#         # sample_id = np.argmin(np.linalg.norm(self.embedded - point, axis=1))
+#         yps = []
+#         yts = []
+#         for sample_id in sample_ids:
+#             yp, yt = (int(self.y_pred[sample_id]), int(self.y_true[sample_id]))
+#             yps.append(yp)
+#             yts.append(yt)
+#         return sample_ids, yps, yts
+#
+#     def show(self):
+#         """Shows the current plot."""
+#         if not hasattr(self, 'fig'):
+#             self._init_plots()
+#
+#         x_min, y_min, x_max, y_max = self._get_plot_measures()
+#
+#         self.cls_plot.set_data(self.classifier_view)
+#         self.cls_plot.set_extent((x_min, x_max, y_max, y_min))
+#         self.ax.set_xlim((x_min, x_max))
+#         self.ax.set_ylim((y_min, y_max))
+#
+#         params_str = 'batch size: %d - n: %d - $\lambda$: %.2f - res: %d'
+#         desc = params_str % (self.batch_size, self.n, self.lam, self.resolution)
+#         self.desc.set_text(desc)
+#
+#         for c in range(self.n_classes):
+#             data = self.embedded[self.y_true == c]
+#             self.sample_plots[c].set_data(data.transpose())
+#
+#         for c in range(self.n_classes):
+#             data = self.embedded[np.logical_and(self.y_pred == c, self.background_at != c)]
+#             self.sample_plots[self.n_classes + c].set_data(data.transpose())
+#
+#         if os.name == 'posix':
+#             self.fig.canvas.manager.window.raise_()
+#
+#         self.selector = SelectFromCollection(self.ax, self.embedded)
+#
+#         self.fig.canvas.draw()
+#         self.fig.canvas.flush_events()
+#         plt.show()
+#
+#     def _init_plots(self):
+#         '''
+#         Initialises matplotlib artists and plots.
+#         '''
+#         if self.interactive:
+#             plt.ion()
+#         self.fig, self.ax = plt.subplots(1, 1, figsize=(8, 8))
+#         self.ax.set_title(self.title)
+#         self.desc = self.fig.text(0.5, 0.02, '', fontsize=8, ha='center')
+#         self.cls_plot = self.ax.imshow(np.zeros([5, 5, 3]),
+#                                        interpolation='gaussian', zorder=0, vmin=0, vmax=1)
+#
+#         self.sample_plots = []
+#
+#         for c in range(self.n_classes):
+#             color = self.cmap(c / (self.n_classes - 1))
+#             plot = self.ax.plot([], [], 'o', label=self.class_dict[c],
+#                                 color=color, zorder=2, picker=mpl.rcParams['lines.markersize'])
+#             self.sample_plots.append(plot[0])
+#
+#         for c in range(self.n_classes):
+#             color = self.cmap(c / (self.n_classes - 1))
+#             plot = self.ax.plot([], [], 'o', markeredgecolor=color,
+#                                 fillstyle='none', ms=12, mew=2.5, zorder=1)
+#             self.sample_plots.append(plot[0])
+#
+#         # set the mouse-event listeners
+#         self.fig.canvas.mpl_connect('key_press_event', self.show_sample)
+#         self.disable_synth = False
+#         self.ax.set_axis_off()
+#         self.ax.legend()
+#
+#
+#     def show_sample(self, event):
+#         '''
+#         Invoked when the user clicks on the plot. Determines the
+#         embedded or synthesised sample at the click location and
+#         passes it to the data_viz method, together with the prediction,
+#         if present a groun truth label and the 2D click location.
+#         '''
+#
+#         # when there is an artist attribute, a
+#         # concrete sample was clicked, otherwise
+#         # show the according synthesised image
+#
+#         if event.key == "enter":
+#             indices = self.selector.ind
+#             sample, p, t = self.get_artist_sample(indices)
+#             # title = '%s <-> %s' if p != t else '%s --- %s'
+#             # title = title % (self.classes[p], self.classes[t])
+#             self.disable_synth = True
+#         elif not self.disable_synth:
+#             # workaraound: inverse embedding needs more points
+#             # otherwise it doens't work --> [point]*5
+#             point = np.array([[event.xdata, event.ydata]] * 5)
+#
+#             # if the outside of the plot was clicked, points are None
+#             if None in point[0]:
+#                 return
+#
+#             sample = self.inverse(point)[0]
+#             sample += abs(sample.min())
+#             sample /= sample.max()
+#             # title = 'Synthesised at [%.1f, %.1f]' % tuple(point[0])
+#             p, t = self.get_mesh_prediction_at(*point[0]), None
+#         else:
+#             self.disable_synth = False
+#             return
+#
+#         if self.data_viz is not None:
+#             self.data_viz(sample, p, t, self.cmap)
+#             return
+#         else:
+#             warnings.warn("Data visualization not possible, as the partnet_grasp points have"
+#                           "no image shape. Pass a function in the data_viz argument,"
+#                           "to enable custom partnet_grasp visualization.")
+#             return
